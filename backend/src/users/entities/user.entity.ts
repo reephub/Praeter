0b769ab945c95dab7c182db65ce173ddb07a2eaa
MichaelStH <michael.sainthonore@gmail.com>
2020-11-22 18:18:09 +0100

    Api User created

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,13 @@
+export class User {
+    
+    gender:string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    phoneNumber: string;
+    dateOfBirth: string;
+    isPremium: boolean;
+    isCustomer:boolean;
+    isProvider: boolean;
+}
