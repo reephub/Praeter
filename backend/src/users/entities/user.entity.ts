02ef3114ba96dd6a3a8a90112a7851c3aeabc6ff
MichaelStH <michael.sainthonore@gmail.com>
2020-11-22 19:21:07 +0100

    User api wip

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,13 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class User {
-    
-    gender:string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    phoneNumber: string;
-    dateOfBirth: string;
-    isPremium: boolean;
-    isCustomer:boolean;
-    isProvider: boolean;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  gender: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  phoneNumber: string;
+
+  @Column()
+  dateOfBirth: string;
+
+  @Column({ default: false })
+  isPremium: boolean;
+
+  @Column({ default: false })
+  isCustomer: boolean;
+
+  @Column({ default: false })
+  isProvider: boolean;
 }
